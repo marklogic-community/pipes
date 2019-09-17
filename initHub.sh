cab1c36edaa15bd79514d13e4fa412179a95701c
Saša Mitrović <sasha.mitrovich@marklogic.com>
2019-09-17 13:28:54 +0200

    VPP-21 added sample data and entities, modified the initHub.sh script accordingly

--- initHub.sh
+++ initHub.sh
@@ -1,5 +1,11 @@
-#initaite DHF
+# initaite DHF
 dhf/gradlew hubInit -p dhf
 
-#copy distro files to DHF folder structure
+# copy distro files to DHF src folder structure
 dhf/gradlew copyDhfDistro -p dhf
+
+# copy DHF entities to DHF entities folder
+dhf/gradlew copyDhfEntities -p dhf
+
+# unpack sample input data
+dhf/gradlew unpackSampleData -p dhf
