c17b5ef11e01c27ca4c1454d3739cea227117535
epoilvet <#XB0m2019&>
2019-11-25 17:03:58 +0100

    Disclaimer

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,34 @@
   <q-page class="flex fixed-center">
 
 <mllitegraph></mllitegraph>
+    <q-dialog v-model="startup">
+      <q-card>
+        <q-toolbar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
 
+          <q-toolbar-title><span class="text-weight-bold">Disclaimer</span> Visual Programming Plugin</q-toolbar-title>
+
+          <q-btn flat round dense icon="close" v-close-popup />
+        </q-toolbar>
+
+        <q-card-section>
+          Visual Programming Plugin (VPP) for MarkLogic DataHub is a community tool.<br/>
+          As such, <b>VPP is not supported by MarkLogic Corporation</b> and is only updated and corrected based on best effort approach.
+          Any contribution or feedback is welcomed to make the tool better.<br/>
+          <b>VPP Team</b>
+          <br/>
+          <br/>
+          Version 0.8 / What's new :
+          <ul>
+            <li>New highlight block on string</li>
+            <li>New entity enrichment block on string</li>
+            <li>New XML and Json schema validation block</li>
+          </ul>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 <script>
@@ -11,7 +38,9 @@
   export default {
     // name: 'ComponentName',
     data () {
-      return {}
+      return {
+          startup:true
+      }
     },
     components: {
       mllitegraph,
