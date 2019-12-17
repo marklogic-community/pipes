883990eabe05821e0900b117156ff4610a25d4e2
Saša Mitrović <sasha.mitrovich@marklogic.com>
2019-12-17 19:04:24 +0100

    VPP-53 the build script now complete

--- buildPackageUiJar.sh
+++ buildPackageUiJar.sh
@@ -1,4 +1,8 @@
+# use this sceipt to generate the JAR file containing the VPP UI
+# it will appear in java-middle-tier/build/libs
+# the version is controlled in java-middle-tier/build.gradle
+
 #!/bin/sh
-quasar dev
+quasar build
 cp -r dist/spa/* java-middle-tier/src/main/resources/static/.
-java-middle-tier/gradle buildJar -p java-middle-tier
+java-middle-tier/gradlew bootJar -p java-middle-tier
