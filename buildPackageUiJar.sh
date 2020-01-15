e9d3c1a2c7952ca0a1b0334f88173d0fdacc7ebe
Saša Mitrović <sasha.mitrovich@marklogic.com>
2020-01-15 10:35:53 +0100

    #7 - now copying the litegraph override module

--- buildPackageUiJar.sh
+++ buildPackageUiJar.sh
@@ -5,28 +5,35 @@
 #!/bin/sh
 
 # build the front end SPA UI
+
+echo "Packaging the front end..."
+cp node_modules_override/litegraph.js node_modules/litegraph.js/build/litegraph.js
 quasar build
 
 # create static resource directory first
+echo "Moving the front-end package to SpringBoot static resources folder..."
 mkdir -p java-middle-tier/src/main/resources/static
 
 # deploy to resources/static to be picked up by jar builder
 cp -r dist/spa/* java-middle-tier/src/main/resources/static/.
 
-
+echo "Moving the back-end modules to SpringBoot dhf resources folder..."
 # create dhf resource directory first
 mkdir -p java-middle-tier/src/main/resources/dhf
 
 # deploy backend modules to be picked up by jar builder
 cp -r dist_user/dhf/* java-middle-tier/src/main/resources/dhf/.
 
+echo "Deleting existing builds in java-middle-tier/build/libs..."
 rm -f java-middle-tier/build/libs/*
-java-middle-tier/gradlew bootJar -p java-middle-tier
+
+echo "Building the jar..."
+java-middle-tier/gradlew bootJar -p java-middle-tier --warn
 
 
 if [[ $1 !=  "release" ]]
   then
-    echo "Assuming nightly build, will add timestamp."
+    echo "Assuming nightly build, will add timestamp..."
 
     for file in java-middle-tier/build/libs/marklogic-pipes*.jar; do
     mv "$file" "${file%.jar}.$(date +%s).jar"
