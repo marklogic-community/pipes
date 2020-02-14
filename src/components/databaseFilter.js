9b04f0c9345eabc61f94f78da1d502360606df2b
Daniel Holgate <dholgate@gmail.com>
2020-02-14 13:13:33 +0100

    Improved db list filtering and now names (alphabetical)
    better filtering of off-limits databases, sorting drop down list

--- src/components/databaseFilter.js
+++ src/components/databaseFilter.js
@@ -1,24 +1,54 @@
 // Copyright ©2020 MarkLogic Corporation.
 const DatabaseFilter = {
-  data() {},
+  
+  data() { }
+,
   methods: {
 
 // Parse out code, config, security, and schemas DBs. Should be off limits for Pipes to write into 
     filterDatabases: function(databases) {
-        // Any code, config, security, schemas DBs should be off limits for Pipes to write into 
-        const offLimitDatabases = ["Last-Login","Schemas","Triggers","Security","App-Services","Modules",
-        "data-hub-staging-SCHEMAS","data-hub-staging-TRIGGERS","data-hub-final-SCHEMAS","data-hub-final-TRIGGERS","Meters","Extensions",
-        "data-hub-MODULES","Fab"]
+
+   var offLimitDatabaseConfig =
+   [
+      { matchType: "endsIn", values: [ "-TRIGGERS","-SCHEMAS", "-MODULES"]},
+      { matchType: "exact",  values: ["Last-Login", "Triggers","Security","Modules","Fab","Extensions","Meters","Schemas","App-Services"] }
+   ]
+
             var filtered = []
             if ( databases !== null && typeof databases === 'object' && databases.length >= 1) {
+
               filtered = databases.filter(
-                database => ( (offLimitDatabases.indexOf(database.label) == -1) ||
-                database.label.endsWith("-TRIGGERS","-MODULES") // for DHF projects with custom DB names
-                )  
+                    database => ( ! this.dbNameMatches(database.label,offLimitDatabaseConfig) )  
                 )
             }
-            return filtered;
+            return filtered.sort()
         },
+
+ dbNameMatches(dbName, offLimitDatabaseConfig) {
+
+  var match = false;
+
+   for (var i = 0; i < offLimitDatabaseConfig.length; i++) {
+     for (var v = 0; v < offLimitDatabaseConfig[i].values.length; v++) {
+        switch (offLimitDatabaseConfig[i].matchType) {
+          case "endsIn":
+         if (dbName.endsWith(offLimitDatabaseConfig[i].values[v])) {
+            match = true;
+            return match
+         }
+          break;
+         case "exact":
+         if (dbName == offLimitDatabaseConfig[i].values[v]) {
+            match = true;
+            return match
+        }
+        break;
+        default:
+           }
+       }
+   }
+   return match
+ }
 
 }
 
