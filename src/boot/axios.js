bed80d3b1389159def4a6ab00cb8957badee0ede
Saša Mitrović <sasha.mitrovich@hotmail.com>
2020-05-14 11:00:10 +0200

    #87 handling session expiration

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,5 +1,36 @@
 import axios from 'axios'
+import { Dialog } from 'quasar'
 
-export default async ({ Vue }) => {
+
+export default async ({ Vue, router, store }) => {
   Vue.prototype.$axios = axios
+
+  // Add a response interceptor
+  axios.interceptors.response.use(function (response) {
+    // Do something with response data
+
+    return response;
+  }, function (error) {
+
+    if (401 === error.response.status) {
+      Dialog.create({
+        title: 'Session expired',
+        message: 'You will have to log in again.',
+        persistent: true
+
+      }).onOk(() => {
+        store.dispatch('authenticated', { auth: false }).then(() => {
+          // this.$q.loading.hide()
+          router.push({ name: "login" })
+        })
+      })
+
+    }
+    // Do something with response error
+
+
+
+    return Promise.reject(error);
+  });
 }
+
