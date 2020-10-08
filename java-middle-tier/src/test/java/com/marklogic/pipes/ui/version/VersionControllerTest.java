dba6661036e0ad5ad70c3e5da9cb4e6efefa2390
Jos van Roosmalen <josr@josr.eu>
2020-10-08 10:16:22 +0200

    Version test

--- java-middle-tier/src/test/java/com/marklogic/pipes/ui/version/VersionControllerTest.java
+++ java-middle-tier/src/test/java/com/marklogic/pipes/ui/version/VersionControllerTest.java
@@ -6,6 +6,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.equalToCompressingWhiteSpace;
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,11 +34,17 @@ class VersionControllerTest {
   @Test
   void testResponse() throws Exception {
 
-    String expectedResponse="Pipes version: 1.0.1-dev-build-1-g5620a6a\n" +
-      "Build: 5620a6a\n";
+    String expectedResponse=readVersionFile();
 
     this.mockMvc.perform(get("/version"))
       .andExpect(content().string(equalToCompressingWhiteSpace(expectedResponse)));
   }
 
+  private static String readVersionFile() throws IOException {
+    StringBuilder buf = new StringBuilder();
+    try (Stream<String> stream = Files.lines( Paths.get("src/main/resources/version.txt"), StandardCharsets.UTF_8))  {
+      stream.forEach(s -> buf.append(s).append("\n"));
+    }
+    return buf.toString();
+  }
 }
